877363fa8926356a63b7405177fb9c7423c5bf49
Piyush Shukla <74832139+panditofcodes@users.noreply.github.com>
2023-08-16 01:46:57 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.get('*', (_, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log('Server is Live!');
